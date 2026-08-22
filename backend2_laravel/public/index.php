b8fa5ae08848dfabf9ca8c16eb2a86d92faaba9e
Carlos <carlos.a.h.palma@gmail.com>
2026-08-21 23:50:27 -0500

    mancoCapac

--- backend2_laravel/public/index.php
+++ backend2_laravel/public/index.php
@@ -1,5 +1,18 @@
 <?php
 
+// Manejo universal de CORS a nivel de entrada HTTP
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With, Application, X-Auth-Token");
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Max-Age: 86400");
+    header("Content-Length: 0");
+    header("Content-Type: text/plain");
+    http_response_code(204);
+    exit(0);
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
